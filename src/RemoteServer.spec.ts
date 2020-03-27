251282c5915233d18ff94dad0b06dfac68df1b01
PhilippLgh <philipp@ethereum.org>
2020-03-27 20:22:11 +0100

    simplify remote server tests

--- src/RemoteServer.spec.ts
+++ src/RemoteServer.spec.ts
@@ -1,34 +1,10 @@
 import { assert } from 'chai'
 import { RemoteServer } from './RemoteServer'
 import { Foo, Bar } from './Fixtures/Foo'
-import { SyncRemoteClient, AsyncRemoteClient } from './Fixtures/Fake'
+import { AsyncRemoteClient } from './Fixtures/Fake'
 
 describe('Remote Server', function () {
-  this.timeout(30 * 1000)
-
-  describe.skip('Sync Client<->Server Communication', () => {
-    it('should handle sync com', async () => {
-
-      const remoteServer = new RemoteServer()
-      remoteServer.exposeObject(new Foo(), 'foo')
-      // console.log('meta', JSON.stringify(meta))
-
-      const remoteClient = new SyncRemoteClient(remoteServer)
-
-      const foo = remoteClient.getObject('foo') as Foo
-
-      assert.equal(foo.name, 'foo')
-
-      const number = await foo.getNumberPromise(42)
-      assert.equal(number, 42)
-
-      const bar = await foo.getBar() as Bar
-      assert.equal(bar.type, 'bar')
-
-      const result = await bar.doSomething()
-      assert.equal(result, 'did something')
-    })
-  })
+  this.timeout(10 * 1000)
 
   describe('Async Client<->Server Communication', () => {
     it('should handle async com', async () => {
@@ -38,18 +14,20 @@ describe('Remote Server', function () {
 
       const remoteClient = new AsyncRemoteClient(remoteServer)
 
-      const foo = await remoteClient.getObject('foo') as Foo
+      const foo = await remoteClient.getRemote('foo') as Foo
 
       assert.equal(await foo.name, 'foo')
 
-      const number = await foo.getNumberPromise(42)
-      assert.equal(number, 42)
+      const number = await foo.incrementNumberAsync(42)
+      assert.equal(number, 43)
+      /*
 
       const bar = await foo.getBar() as Bar
       assert.equal(await bar.type, 'bar')
 
       const result = await bar.doSomething()
       assert.equal(result, 'did something')
+      */
     })
   })
 
